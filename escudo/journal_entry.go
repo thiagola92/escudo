7ade7a320a1f1ad488a24d5e4045883161ee0a61
thiagola92 <thiagola92@gmail.com>
2025-11-10 13:56:36 -0300

    Move status to journal.go

--- escudo/journal_entry.go
+++ escudo/journal_entry.go
@@ -1,36 +1,5 @@
 package escudo
 
-const (
-	// When setted means that we need to create the lock file
-	// (and maybe the temporary file too).
-	//
-	// Finding this state in the journal means that we can't recover
-	// from the crash and we should just delete the lock file.
-	//
-	// NOTE: This state is never stored in the journal file.
-	INITIALIZING = 0
-
-	// When setted means that the user can edit the temporary file
-	// as they wish.
-	//
-	// Finding this state in the journal means that we can't recover
-	// from the crash and we should just delete the temporary & lock files.
-	WRITING = 100
-
-	// When setted means that we need to replace the original file
-	// with the temporary file.
-	//
-	// Finding this state in the journal means that we CAN recover
-	// from the crash and we should continue from where we stopped.
-	REPLACING = 200
-
-	// When setted means that we need to delete the temporary file.
-	//
-	// Finding this state in the journal means that we should
-	// continue from where we stopped.
-	DELETING = 300
-)
-
 type JournalEntry struct {
 	file *File
 
